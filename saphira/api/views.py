3ad22c165bd4955778577d800ed2df9f89ea5136
FranciscoGJR <franciscoogjr@gmail.com>
2024-04-23 01:22:21 -0300

    refactor: remover JsonResponse

--- saphira/api/views.py
+++ saphira/api/views.py
@@ -3,4 +3,4 @@ import datetime
 from django.http import HttpResponse, JsonResponse
 
 def get_datetime(request):
-    return JsonResponse({"datetime": datetime.datetime.utcnow()})
+    return HttpResponse(datetime.datetime.utcnow())
